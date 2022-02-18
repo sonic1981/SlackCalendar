984793af73e68acd8dd45a187be042e8bf7fbbb5
liam.hughes@ivanti.com <liam.hughes@ivanti.com>
2022-02-18 16:58:35 +0000

    Setup simple go function

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"example.com/hello"
+	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
+)
+
+func main() {
+	ctx := context.Background()
+	if err := funcframework.RegisterHTTPFunctionContext(ctx, "/", hello.HelloWorld); err != nil {
+		log.Fatalf("funcframework.RegisterHTTPFunctionContext: %v\n", err)
+	}
+	// Use PORT environment variable, or default to 8080.
+	port := "1981"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
+	if err := funcframework.Start(port); err != nil {
+		log.Fatalf("funcframework.Start: %v\n", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module github.com/sonic1981/SlackCalendar
+module example.com/hello
 
 go 1.16
 
 require (
 	cloud.google.com/go/pubsub v1.18.0 // indirect
+	github.com/GoogleCloudPlatform/functions-framework-go v1.5.3 // indirect
 	github.com/slack-go/slack v0.10.2 // indirect
 )
