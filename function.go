ac050c36ff015de4b4f01870b74ad694ebbee753
liam.hughes@ivanti.com <liam.hughes@ivanti.com>
2022-03-04 21:36:44 +0000

    Simple http call

--- function.go
+++ function.go
@@ -2,10 +2,28 @@ package hello
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"time"
 )
+
+var netClient = &http.Client{
+	Timeout: time.Second * 10,
+}
 
 // HelloWorld writes "Hello, World!" to the HTTP response.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, World!\n")
+
+	resp, err := netClient.Get("http://example.com/")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	fmt.Fprint(w, string(body))
 }
